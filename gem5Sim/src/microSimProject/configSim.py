b2fd70edf6a943a4d29f5f8c5cbaddb6dadecb89
tunjijnr1 <51492794+tunjijnr1@users.noreply.github.com>
2021-04-13 19:43:33 -0400

    Added comments and created the memory bus

--- gem5Sim/src/microSimProject/configSim.py
+++ gem5Sim/src/microSimProject/configSim.py
@@ -1,16 +1,21 @@
 import m5
 from m5.objects import *
 
-system = System()
+system = System() #Created the system Object which is the parent of all other objects in our simulated system.
 
-system.clk_domain = SrcClockDomain()
-system.clk_domain.clock = '1GHz'
-system.clk_domain.voltage_domain = VoltageDomain()
+system.clk_domain = SrcClockDomain() #created the clock domain
+system.clk_domain.clock = '1GHz' #Set the clock to 1GHz
+system.clk_domain.voltage_domain = VoltageDomain() # Set the voltage domain for the clock to default option
 
-system.mem_mode = 'timing'
-system.mem_ranges = [AddrRange('512MB')]
+system.mem_mode = 'timing' #Used timing mode for the memory simulation
+system.mem_ranges = [AddrRange('512MB')] #Set the single memory range to size 512MB
+
+system.cpu = TimingSimpleCPU() #Created the CPU which executes each instruction in a single clock cycle except for the memory request
+
+system.membus = SystemXBar() #Created the system wide memory bus
 
 root = Root(full_system = False)
+
 
 root.svsgd = SevenSegDis()
 root.svsgd.toDisplay = "A"
