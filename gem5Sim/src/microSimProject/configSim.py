6e84a7915b8b78f2cfe357bef85453a373287384
tunjijnr1 <51492794+tunjijnr1@users.noreply.github.com>
2021-04-14 09:57:52 -0400

    Added new test cases
    They all passed the test

--- gem5Sim/src/microSimProject/configSim.py
+++ gem5Sim/src/microSimProject/configSim.py
@@ -31,6 +31,46 @@ system.mem_ctrl.port = system.membus.master
 #Created the root object for the instantiation of the system and beginning of execution
 root = Root(full_system = False)
 
+#Funbi's test
+#root.svsgd = SevenSegDis()
+#root.svsgd.toDisplay = "A"
+#root.svsgd2 = SevenSegDis()
+#root.svsgd2.toDisplay = "3"
+#root.svsgd3 = SevenSegDis()
+#root.svsgd3.toDisplay = "b"
+#root.svsgd4 = SevenSegDis()
+#root.svsgd4.toDisplay = "0"
+#root.svsgd5 = SevenSegDis()
+#root.svsgd5.toDisplay = "C"
+#root.svsgd6 = SevenSegDis()
+#root.svsgd6.toDisplay = "4"
+#root.svsgd7 = SevenSegDis()
+#root.svsgd7.toDisplay = "d"
+#root.svsgd8 = SevenSegDis()
+#root.svsgd8.toDisplay = "2"
+#root.svsgd9 = SevenSegDis()
+#root.svsgd9.toDisplay = "E"
+#root.svsgd10 = SevenSegDis()
+#root.svsgd10.toDisplay = "1"
+
+#Funbi's test
+#root.tester = Sensor()
+#root.tester.curTemp = (random.randint(0, 150))
+#root.tester.newTemp = (random.randint(0, 150))
+#root.tester2 = Sensor()
+#root.tester2.curTemp = (random.randint(0, 150))
+#root.tester2.newTemp = (random.randint(0, 150))
+#root.tester3 = Sensor()
+#root.tester3.curTemp = (random.randint(0, 150))
+#root.tester3.newTemp = (random.randint(0, 150))
+#root.tester4 = Sensor()
+#root.tester4.curTemp = (random.randint(0, 150))
+#root.tester4.newTemp = (random.randint(0, 150))
+#root.tester5 = Sensor()
+#root.tester5.curTemp = (random.randint(0, 150))
+#root.tester5.newTemp = (random.randint(0, 150))
+
+#Valerie's Test 
 root.svsgd = SevenSegDis()
 root.svsgd.toDisplay = "0"
 root.svsgd2 = SevenSegDis()
@@ -50,6 +90,7 @@ root.svsgd8.toDisplay = "d"
 root.svsgd9 = SevenSegDis()
 root.svsgd9.toDisplay = "E"
 
+#Valerie's Test 
 root.tester = Sensor()
 root.tester.curTemp = (random.randint(0, 100))
 root.tester.newTemp = (random.randint(0, 100))
@@ -68,7 +109,7 @@ root.tester5.newTemp = (random.randint(0, 100))
 
 m5.instantiate()
 
-print("Beginning simulation!")
+
+print("Beginning simmulation!")
 exit_event = m5.simulate()
 print('Exiting @ tick {} because {}'.format(m5.curTick(), exit_event.getCause()))
-
